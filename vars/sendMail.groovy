dc42c1c2e53433634164f1ec54a3dd9ff66f6e10
Vorkosigan <contact@boureaujm.fr>
2024-07-19 21:50:59 +0200

    complex body

--- vars/sendMail.groovy
+++ vars/sendMail.groovy
@@ -1,3 +1,13 @@
 void call() {
-    emailext attachLog: true, body: '<h1>accentué<h1/>', mimeType: 'text/html', subject: 'subject', to: 'contact@me.com'
+    String body = '''
+    <!DOCTYPE html>
+<html>
+<body>
+
+<h1>accentué</h1>
+<p>My first paragraph.</p>
+
+</body>
+</html>'''
+    emailext attachLog: true, body: body, mimeType: 'text/html', subject: 'subject', to: 'contact@me.com'
 }
